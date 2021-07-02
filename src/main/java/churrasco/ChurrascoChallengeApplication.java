0410367b0c4d74c8af8c9335588e5fba594f7f56
Melof10 <95fedemelo@gmail.com>
2021-07-02 04:53:39 -0300

    first commit

---
+++ src/main/java/churrasco/ChurrascoChallengeApplication.java
@@ -0,0 +1,13 @@
+package churrasco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurrascoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurrascoChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/churrasco/ChurrascoChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package churrasco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurrascoChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
