bb055be16b9269e21d9720c46312f106bd941da0
Melof10 <95fedemelo@gmail.com>
2021-07-02 05:29:42 -0300

    Created controller for get all products

---
+++ src/main/java/churrasco/controllers/ProductController.java
@@ -0,0 +1,33 @@
+package churrasco.controllers;
+
+import churrasco.entities.Product;
+import churrasco.services.IProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+    
+    @Autowired
+    private IProductService iProductService;
+    
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<Product>> findAll() throws Exception {
+        try {
+            List<Product> products = iProductService.findAll();
+            
+            if(!products.isEmpty())
+                return new ResponseEntity<>(products, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                        
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+}
