06ce76acb0627b4edd721a1985d6e94d0dad9d6d
Melof10 <95fedemelo@gmail.com>
2021-07-04 17:24:25 -0300

    Added service for user

---
+++ src/main/java/churrasco/security/services/UserService.java
@@ -0,0 +1,37 @@
+package churrasco.security.services;
+
+import churrasco.security.entities.User;
+import churrasco.security.repository.IUserRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UserService {
+    
+    @Autowired
+    private IUserRepository iUserRepository;
+    
+    public Optional<User> getByUsername(String username){
+        return iUserRepository.findByUsername(username);
+    }
+    
+    public Optional<User> getByEmail(String email){
+        return iUserRepository.findByEmail(email);
+    }
+    
+    public boolean existsByUsername(String username){
+        return iUserRepository.existsByUsername(username);
+    }
+    
+    public boolean existsByEmail(String email){
+        return iUserRepository.existsByEmail(email);
+    }
+    
+    public void save(User user){
+        iUserRepository.save(user);
+    }
+    
+}
