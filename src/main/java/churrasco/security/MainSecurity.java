146e92014896c1ae8a356439dae2d7d08566d09b
Melof10 <95fedemelo@gmail.com>
2021-07-04 18:25:23 -0300

    Implemented main security

--- src/main/java/churrasco/security/MainSecurity.java
+++ src/main/java/churrasco/security/MainSecurity.java
@@ -1,5 +1,60 @@
 package churrasco.security;
 
-public class MainSecurity {
+import churrasco.security.jwt.JwtEntryPoint;
+import churrasco.security.jwt.JwtTokenFilter;
+import churrasco.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MainSecurity extends WebSecurityConfigurerAdapter{
+    
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+    
+    @Autowired
+    private JwtEntryPoint jwtEntryPoint;
+    
+    @Bean
+    public JwtTokenFilter jwtTokenFilter(){
+        return new JwtTokenFilter();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+    }
+    
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
     
 }
