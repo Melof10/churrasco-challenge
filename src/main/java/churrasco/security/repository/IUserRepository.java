6a5b70ca75f1899bdf067d1a35816bbb9dcabdb1
Melof10 <95fedemelo@gmail.com>
2021-07-04 17:18:26 -0300

    Added repository for user

---
+++ src/main/java/churrasco/security/repository/IUserRepository.java
@@ -0,0 +1,19 @@
+package churrasco.security.repository;
+
+import churrasco.security.entities.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long>{
+    
+    public Optional<User> findByUsername(String username);
+    
+    public Optional<User> findByEmail(String email);
+    
+    public boolean existsByUsername(String username);
+    
+    public boolean existsByEmail(String email);
+    
+}
